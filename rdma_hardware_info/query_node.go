4bfba2963b5226388c94024eef568c6da3c6309d
cal8384 <cal8384@rit.edu>
2019-07-02 13:30:45 -0400

    Fixed import statements and intra-package references in daemonset querying function.

--- rdma_hardware_info/query_node.go
+++ rdma_hardware_info/query_node.go
@@ -1,29 +1,33 @@
 package rdma_hardware_info
 
 import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
 	"net/http"
+	"time"
 )
 
 
-func QueryNode(node_address string, port string, timeout_ms int) ([]rdma_hardware_info.PF, error) {
+func QueryNode(node_address string, port string, timeout_ms int) ([]PF, error) {
 	http_client := http.Client {
-		Timeout: time.Duration(timeout_ms * time.Millisecond),
+		Timeout: time.Duration(time.Duration(timeout_ms) * time.Millisecond),
 	}
 
 	resp, err := http_client.Get(fmt.Sprintf("http://%s:%s/%s", node_address, port, RdmaInfoUrl))
 	if(err != nil) {
-		return []rdma_hardware_info.PF{}, err
+		return []PF{}, err
 	}
 
 	data, err := ioutil.ReadAll(resp.Body)
 	if(err != nil) {
-		return []rdma_hardware_info.PF{}, err
+		return []PF{}, err
 	}
 
-	var pfs []rdma_hardware_info.PF
+	var pfs []PF
 	err = json.Unmarshal(data, &pfs)
 	if(err != nil) {
-		return []rdma_hardware_info.PF{}, err
+		return []PF{}, err
 	}
 
 	return pfs, nil
